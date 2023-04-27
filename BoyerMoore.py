9e0f102bcfaf59d36a8f270401d8a330a656e2bd
umut373 <umut373.uz@gmail.com>
2023-04-27 17:13:28 +0300

    generating good suffix table is done

--- BoyerMoore.py
+++ BoyerMoore.py
@@ -3,6 +3,36 @@ def generate_bad_symbol_table() :
         bad_symbol[pattern[i]] = p_length - i - 1
 
 
+def generate_good_suffix_table() :
+    i = p_length 
+    j = p_length + 1
+    bpos = [0] * (p_length + 1)
+    bpos[i] = j
+
+    while i > 0 :
+        while j <= p_length and pattern[i-1] != pattern[j-1] :
+            if good_suffix[p_length-j] == 0 :
+                good_suffix[p_length-j] = j - i
+
+            j = bpos[j]
+        
+        i -= 1
+        j -= 1
+        bpos[i] = j
+
+
+    j = bpos[0]
+    for i in range(p_length  + 1) :
+        if good_suffix[p_length-i] == 0 :
+            good_suffix[p_length-i] = j
+
+        if i == j :
+            j = bpos[j]
+
+    good_suffix[p_length] = p_length
+
+
+
 if __name__ == "__main__" :
     text = "WHICH_FINALLY_HALTS. _ _ AT_THAT POINT"
     pattern = "AT_THAT"
@@ -11,7 +41,13 @@ if __name__ == "__main__" :
     p_length = len(pattern)
 
     bad_symbol = {}
+    good_suffix = [0] * (p_length + 1)
+
     generate_bad_symbol_table()
+    generate_good_suffix_table()
 
     for element in bad_symbol.items() :
         print(element)
+
+    for i in good_suffix :
+        print(i)
