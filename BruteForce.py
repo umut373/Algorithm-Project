c07dcceeec833c0b07cca22532da2cfe5a9fe653
egekeklikci <ege.keklikci@gmail.com>
2023-05-11 20:22:33 +0300

    Update BruteForce.py

--- BruteForce.py
+++ BruteForce.py
@@ -1,22 +1,25 @@
 import datetime
+
+import BoyerMoore
+
 count = 0
-def brute_force():
+def brute_force(input, output, patternn):
     index = 0
-    for i in range(len(text) - len(pattern)+1):
-        for j in range(len(pattern)):
-            if pattern[j] == text[j+i]:
-                if j == len(pattern)-1:
-                    output_file.write(text[index: i] + "<MARK>" + pattern + "</MARK>")
+    for i in range(len(input) - len(patternn)+1):
+        for j in range(len(patternn)):
+            if patternn[j] == input[j+i]:
+                if j == len(patternn)-1:
+                    output.write(input[index: i] + "<MARK>" + patternn + "</MARK>")
                     index = i + j + 1
                     global count
                     count += 1
             else:
                 break
-    output_file.write(text[index:len(text)])
+    output.write(input[index:len(input)])
 
 
 if __name__ == "__main__":
-    input_file = open("random_input_120k.html", "r")
+    input_file = open("random_input_1m.html", "r")
     output_file = open("output.html", "w")
 
     text = input_file.readline()
@@ -24,6 +27,6 @@ if __name__ == "__main__":
     pattern = input("Enter a pattern: ")
 
     start_time = datetime.datetime.now()
-    brute_force()
+    brute_force_count = brute_force(text, output_file, pattern)
     exec_time = datetime.datetime.now() - start_time
-    print(count,"matches found\nExecution time is", exec_time.microseconds)
+    print(count, "matches found\nExecution time is", exec_time.microseconds)
