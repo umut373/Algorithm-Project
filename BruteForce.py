300ad43f9d246a3178783e7cec78c45922ef579d
egekeklikci <ege.keklikci@gmail.com>
2023-05-11 22:21:08 +0300

    Added indexing

--- BruteForce.py
+++ BruteForce.py
@@ -2,25 +2,52 @@ import datetime
 number_of_compression = 0
 
 def brute_force(input, output, patternn):
-    index = 0
     count = 0
+    indexes = []
+    a = 0
     global number_of_compression
     for i in range(len(input) - len(patternn) + 1):
         for j in range(len(patternn)):
             number_of_compression += 1
             if patternn[j] == input[j + i]:
                 if j == len(patternn) - 1:
-                    output.write(input[index: i] + "<MARK>" + patternn + "</MARK>")
-                    index = i + j + 1
+                    indexes.append(i)
                     count += 1
             else:
                 break
-    output.write(input[index:len(input)])
+    output_list = list(input)
+
+    j = 0
+    counter = 0
+    continued = False
+    while j < len(indexes):
+        if not continued:
+            if j == 0:
+                output_list.insert(indexes[j] + counter, "<MARK>")
+                counter += 1
+            else:
+                output_list.insert(indexes[j] + counter, "<MARK>")
+                counter += 1
+                output_list.insert(indexes[j-1] + len(patternn) + counter-1, "</MARK>")
+                counter += 1
+            continued = True
+        else:
+            if indexes[j] > indexes[j-1]+len(patternn):
+                output_list.insert(indexes[j-1]+len(patternn)+counter, "</MARK>")
+                continued = False
+                counter += 1
+                output_list.insert(indexes[j] + counter, "<MARK>")
+                counter += 1
+        j += 1
+    if len(indexes) != 0:
+        output_list.insert(indexes[-1]+len(patternn)+counter, "</MARK>")
+    output.write("".join(output_list)
+)
     return count
 
 
 if __name__ == "__main__":
-    input_file = open("random_input_1m.html", "r")
+    input_file = open("random_input_1000.html", "r")
     output_file = open("output.html", "w")
 
     text = input_file.readline()
