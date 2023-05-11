72071fea1c9b7de69e7f532acd0b8e34ef26a48f
egekeklikci <ege.keklikci@gmail.com>
2023-05-11 20:24:14 +0300

    Update BruteForce.py

--- BruteForce.py
+++ BruteForce.py
@@ -2,21 +2,19 @@ import datetime
 
 import BoyerMoore
 
-count = 0
 def brute_force(input, output, patternn):
-    index = 0
+    index = 0; count = 0
     for i in range(len(input) - len(patternn)+1):
         for j in range(len(patternn)):
             if patternn[j] == input[j+i]:
                 if j == len(patternn)-1:
                     output.write(input[index: i] + "<MARK>" + patternn + "</MARK>")
                     index = i + j + 1
-                    global count
                     count += 1
             else:
                 break
     output.write(input[index:len(input)])
-
+    return count
 
 if __name__ == "__main__":
     input_file = open("random_input_1m.html", "r")
@@ -29,4 +27,4 @@ if __name__ == "__main__":
     start_time = datetime.datetime.now()
     brute_force_count = brute_force(text, output_file, pattern)
     exec_time = datetime.datetime.now() - start_time
-    print(count, "matches found\nExecution time is", exec_time.microseconds)
+    print(brute_force_count, "matches found\nExecution time is", exec_time.microseconds)
